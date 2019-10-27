cee17b431c85f7c5ba545b1080c5244ed66e0ae6
Kaveh Mousavi Zamani <kmousavizamani@wayfair.com>
2019-10-27 22:41:10 +0100

    setup kubeadm

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -1,13 +1,48 @@
-user { 'mitchell':
-  ensure     => 'absent',
-  uid        => '1000',
-  gid        => '1000',
-  shell      => '/bin/bash',
-  home       => '/home/mitchell',
-  managehome => true,
-}
+node default {
+  package { 'iptables':
+    ensure => installed,
+    name   => 'iptables',
+  }
+  package { 'arptables':
+    ensure => installed,
+    name   => 'arptables',
+  }
+  package { 'ebtables':
+    ensure => installed,
+    name   => 'ebtables',
+  }
 
-group { 'mitchell':
-  ensure => 'present',
-  gid    => '1000',
+  alternatives { 'iptables':
+    path => '/usr/sbin/iptables-legacy',
+  }
+  alternatives { 'ip6tables':
+    path => '/usr/sbin/ip6tables-legacy',
+  }
+  alternatives { 'arptables':
+    path => '/usr/sbin/arptables-legacy',
+  }
+  alternatives { 'ebtables':
+    path => '/usr/sbin/ebtables-legacy',
+  }
+
+  include 'docker'
+
+
+  package { 'apt-transport-https':
+    ensure => installed,
+    name   => 'apt-transport-https',
+  }
+  package { 'curl':
+    ensure => installed,
+    name   => 'curl',
+  }
+
+  exec { '/usr/bin/curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | /usr/bin/apt-key add -': }
+  exec { '/usr/bin/echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list': }
+  exec { '/usr/bin/apt-get update': }
+  exec { '/usr/bin/apt-get install -y kubelet kubeadm kubectl': }
+  exec { '/usr/bin/apt-mark hold kubelet kubeadm kubectl': }
+
+  exec { '/usr/bin/systemctl daemon-reload': }
+  exec { '/usr/bin/systemctl restart kubelet': }
 }
